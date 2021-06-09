ca027bd0260b6b54bc94306c383918dbc96743d7
hectorgomez74343 <hmg701@yahoo.com>
2021-06-09 07:53:31 -0500

    Migrated from create-react-app to webpack

---
+++ src/components/Scheduler.js
@@ -0,0 +1,169 @@
+import React from "react";
+import { withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+
+import { addOrder } from "../redux/actions/ordersActions";
+import "./Scheduler.css";
+
+class Scheduler extends React.PureComponent {
+  state = {
+    address: "",
+    email: "",
+    name: "",
+    lastName: "",
+    error:
+      "You need to enter your first name, last name, phone number, address and email to order a service.",
+    formError: false,
+    phoneNumber: "",
+  };
+  routeToRoot = () => {
+    const { history } = this.props;
+    history.push("/");
+  };
+  confirmOrder = () => {
+    const { history, addOrdr } = this.props;
+    const { title } = this.props.orderType;
+    const date = new Date();
+    const { name, lastName, phoneNumber, email, address } = this.state;
+    if (
+      name.length === 0 ||
+      lastName.length === 0 ||
+      phoneNumber.length === 0 ||
+      email.length === 0 ||
+      address.length === 0
+    ) {
+      return this.setState({
+        formError: true,
+      });
+    }
+    addOrdr({
+      name,
+      lastName,
+      id: Math.floor(100000 + Math.random() * 900000),
+      time: date.toString(),
+      type: title,
+      phoneNumber,
+      email,
+      address,
+    });
+    history.push("/confirmation-order");
+  };
+
+  setValue = (e) => {
+    const { name, value } = e.target;
+
+    this.setState({
+      [name]: value,
+      formError: false,
+    });
+  };
+  render() {
+    const { text, image, title } = this.props.orderType;
+    const {
+      address,
+      email,
+      name,
+      lastName,
+      error,
+      formError,
+      phoneNumber,
+    } = this.state;
+
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h2>Order your service</h2>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col scheduler-flex-box">
+            <button
+              className="btn btn-danger"
+              type="button"
+              onClick={this.routeToRoot}
+            >
+              Click to order a different service
+            </button>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <div className="Cards cards-flex-box">
+              <div className="Card card-dimension" onClick={this.confirmOrder}>
+                <img src={image} alt={title} className="card-image" />
+                <h3>{title}</h3>
+                <h3 className="card-text">{text}</h3>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            <div className="input-flex-box ">
+              <h3 className="inputs-header">
+                Enter your first name, last name, phone number, address, and
+                email.
+              </h3>
+              <input
+                type="text"
+                name="name"
+                placeholder="Enter your name"
+                className="inputs-margin"
+                onChange={this.setValue}
+                value={name}
+              />
+              <input
+                type="text"
+                name="lastName"
+                placeholder="Enter your last name"
+                className="inputs-margin"
+                onChange={this.setValue}
+                value={lastName}
+              />
+              <input
+                type="tel"
+                name="phoneNumber"
+                placeholder="Enter your phone number"
+                className="inputs-margin"
+                onChange={this.setValue}
+                value={phoneNumber}
+              />
+              <input
+                type="text"
+                name="address"
+                placeholder="Enter your address"
+                className="inputs-margin"
+                onChange={this.setValue}
+                value={address}
+              />
+              <input
+                type="text"
+                name="email"
+                placeholder="Enter your email"
+                className="inputs-margin"
+                onChange={this.setValue}
+                value={email}
+              />
+              {formError ? <p className="error-header">{error}</p> : null}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    orderType: state.ordersReducer.orderType,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    addOrdr: (order) => dispatch(addOrder(order)),
+  };
+}
+
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(Scheduler)
+);
